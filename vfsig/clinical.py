cf12c3332ffe49c1bdc5cf307f650a39f49c37f8
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-04-21 11:18:54 -0400

    DOC: Update documentation for `clinical` module

--- vfsig/clinical.py
+++ vfsig/clinical.py
@@ -1,5 +1,6 @@
 """
-This module contains functions to compute classic ratios from common glottal signals
+This module contains functions to compute classic ratios from common vocal fold
+(VF) derived signals
 
 The definitions of these ratios come from a variety of papers including
 E. Holmberg, R. Hillman, J. Perkell - Glottal airflow ... - 1998 - JASA
@@ -7,28 +8,41 @@ E. Holmberg, R. Hillman, J. Perkell - Glottal airflow ... - 1998 - JASA
 
 import numpy as np
 
-# Returns arrays with the same shape as the signal
+## Indicating functions
+# These functions indicate whether the VFs are in some state or not
+# (open, closed, etc..)
 def is_closed(t, y, closed_ub):
     """
-    Return a float array indicating closure
+    Return a boolean array indicating VF closure
     """
     return y < closed_ub
 
 def is_open(t, y, closed_ub):
+    """
+    Return a boolean array indicating VFs are open
+    """
     return np.logical_not(is_closed(t, y, closed_ub))
 
 def is_closing(t, y, closed_ub):
+    """
+    Return a boolean array indicating VFs are closing
+    """
     y_prime = np.gradient(y, t)
     return np.logical_and(is_open(t, y, closed_ub), y_prime < 0)
 
 def is_opening(t, y, closed_ub):
+    """
+    Return a boolean array indicating VFs are opening
+    """
     y_prime = np.gradient(y, t)
     return np.logical_and(is_open(t, y, closed_ub), y_prime >= 0)
 
-# Returns scalar summaries of the signal
+# Return scalar summaries of the signal
 def closed_ratio(t, y, closed_ub=0.0):
     """
-    Return the ratio of closed time to total time over the provided signal
+    Return the closed ratio
+
+    This is the ratio of time spent closed to total time
     """
     ind_closed = np.array(is_closed(t, y, closed_ub), dtype=np.float)
     closed_duration = np.trapz(ind_closed, x=t)
@@ -37,13 +51,17 @@ def closed_ratio(t, y, closed_ub=0.0):
 
 def open_ratio(t, y, closed_ub=0.0):
     """
-    Return the ratio of open time to total time over the provided signal
+    Return the open ratio
+
+    This is the ratio of time spent open to total time
     """
     return 1 - closed_ratio(t, y, closed_ub)
 
 def closing_ratio(t, y, closed_ub=0.0):
     """
-    Return the ratio of closed time to total time over the provided signal
+    Return the closing ratio
+
+    This is the ratio of time spent closing to the total time
     """
     ind_closing = np.array(is_closing(t, y, closed_ub), dtype=np.float)
     closing_duration = np.trapz(ind_closing, x=t)
@@ -52,7 +70,9 @@ def closing_ratio(t, y, closed_ub=0.0):
 
 def opening_ratio(t, y, closed_ub=0.0):
     """
-    Return the ratio of closed time to total time over the provided signal
+    Return the opening ratio
+
+    This is the ratio of time spent opening to the total time
     """
     ind_opening = np.array(is_opening(t, y, closed_ub), dtype=np.float)
     opening_duration = np.trapz(ind_opening, x=t)
@@ -60,6 +80,11 @@ def opening_ratio(t, y, closed_ub=0.0):
     return opening_duration/duration
 
 def speed_ratio(t, y, closed_ub=0.0):
+    """
+    Return the speed ratio
+
+    This is the ratio of opening to closing times
+    """
     return opening_ratio(t, y, closed_ub)/closing_ratio(t, y, closed_ub)
 
 def mfdr(t, y):
@@ -70,6 +95,11 @@ def mfdr(t, y):
     return np.min(yp)
 
 def ac_flow(t, y):
+    """
+    Return the AC flow
+
+    This is the amplitude from minimum to maximum of the signal
+    """
     return np.max(y) - np.min(y)
 
 def acdc(t, y):
