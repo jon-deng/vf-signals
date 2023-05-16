8c5b8b28c1118d032c38ec5280d4c493a73f9e76
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-05-15 21:46:07 -0400

    tests: Add (wip) `pytest` tests for `modal`

--- tests/test_modal.py
+++ tests/test_modal.py
@@ -1,57 +1,65 @@
+"""
+Test the module `modal`
+"""
+
+import pytest
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from vfsig import modal, clinical
+from vfsig import modal
 
-def setup_1d_signal():
-    N_PERIOD = 10.1
-    N_PER_PERIOD = 128
-    N_SAMPLE = int(round(N_PERIOD * N_PER_PERIOD))
+@pytest.fixture()
+def fundamental_period():
+    return 1.0
 
-    DT = 0.01
+@pytest.fixture()
+def fundamental_freq(fundamental_period):
+    return 1/fundamental_period
 
-    t = DT*np.arange(N_SAMPLE)
-    n_per_period = N_SAMPLE/N_PERIOD
-    period = DT * n_per_period
-    y = np.sin(2*np.pi * 1/period * t)
-    return y, DT, int(round(N_PERIOD))
+@pytest.fixture()
+def fundamental_phase():
+    return np.random.rand()*2*np.pi
 
-def setup_2d_signal_a():
-    y, dt, nperiod = setup_1d_signal()
+@pytest.fixture()
+def time(fundamental_period):
+    num_samples_per_period = 10
+    num_periods = 20
 
-    # Return a (5, #samples) signal
-    return y*np.ones((5, 1)), dt, nperiod
+    dt = fundamental_period/num_samples_per_period
+    return dt*np.arange(num_samples_per_period*num_periods)
 
-def setup_2d_signal_b():
-    y, dt, nperiod = setup_1d_signal()
+@pytest.fixture()
+def signal(time, fundamental_period, fundamental_phase):
+    return np.sin(2*np.pi*time/fundamental_period - fundamental_phase)
 
-    # Return a (#samples, 5) signal
-    return y[:, np.newaxis]*np.ones((1, 5)), dt, nperiod
+def test_fundamental_mode_from_rfft(signal, time, fundamental_freq, fundamental_phase):
+    est_fund_freq, dfreq, est_fund_phase, dphase, info = modal.fundamental_mode_from_rfft(
+        signal, time[1]-time[0],
+    )
+    print(f"Estimated FO: {est_fund_freq:.2e} +/- {dfreq:.2e}, actual FO {fundamental_freq:.2e}")
+    print(f"Estimated phiO: {est_fund_phase:.2e} +/- {dphase:.2e}, actual phiO {fundamental_phase:.2e}")
+
+def test_fundamental_mode_from_peaks(signal, time, fundamental_freq, fundamental_phase):
+    est_fund_freq, dfreq, est_fund_phase, dphase, info = modal.fundamental_mode_from_peaks(
+        signal, time[1]-time[0],
+    )
+    print(f"Estimated FO: {est_fund_freq:.2e} +/- {dfreq:.2e}, actual FO {fundamental_freq:.2e}")
+    print(f"Estimated phiO: {est_fund_phase:.2e} +/- {dphase:.2e}, actual phiO {fundamental_phase:.2e}")
 
 
-def test_estimate_periodic_statistics(y, n_period):
-    mean_y, stdev_y = modal.estimate_periodic_statistics(y, n_period)
+# def test_estimate_periodic_statistics(y, n_period):
+#     mean_y, stdev_y = modal.periodic_stats(y, n_period)
 
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(mean_y, color='b', ls='-')
-    ax.plot(mean_y+stdev_y*3, color='b', ls='-.')
-    ax.plot(mean_y-stdev_y*3, color='b', ls='-.')
-    ax.set_xlabel("Time []")
-    ax.set_ylabel("Signal []")
-    fig.savefig('fig/test_estimate_periodic_statistics.png')
+#     fig, ax = plt.subplots(1, 1)
+#     ax.plot(mean_y, color='b', ls='-')
+#     ax.plot(mean_y+stdev_y*3, color='b', ls='-.')
+#     ax.plot(mean_y-stdev_y*3, color='b', ls='-.')
+#     ax.set_xlabel("Time []")
+#     ax.set_ylabel("Signal []")
+#     fig.savefig('fig/test_estimate_periodic_statistics.png')
 
-def test_estimate_fundamental_mode():
-    y, dt, _ = setup_1d_signal()
-    fund_f, fund_phase, df = modal.estimate_fundamental_mode(y, dt, axis=-1)
-    print(fund_f.shape)
 
-    y, dt, _ = setup_2d_signal_a()
-    fund_f, fund_phase, df = modal.estimate_fundamental_mode(y, dt, axis=-1)
-    print(fund_f.shape)
-
-    y, dt, _ = setup_2d_signal_b()
-    fund_f, fund_phase, df = modal.estimate_fundamental_mode(y, dt, axis=-2)
-    print(fund_f.shape)
 
 if __name__ == '__main__':
     y, dt, n_period = setup_1d_signal()
